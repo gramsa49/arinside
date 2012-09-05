e2429b8dd328dda5c0c0f89a03f6def2c92beb31
jls17 <jls17@1b5bf968-bca2-4794-aaf2-a93241d7ae32>
2012-09-05 17:52:55 +0000

    - added istream and ostream support-files to rapidjson (see rapidjson issue 20 http://code.google.com/p/rapidjson/issues/detail?id=20)
    git-svn-id: http://svn.arinside.org/branches/work_304@314 1b5bf968-bca2-4794-aaf2-a93241d7ae32

--- RapidJSONTests.cpp
+++ RapidJSONTests.cpp
@@ -2,6 +2,7 @@
 #include "rapidjson/document.h"
 #include "rapidjson/filestream.h"
 #include "rapidjson/prettywriter.h"
+#include "rapidjson/genericwritestream.h"
 
 using namespace rapidjson;
 
@@ -25,8 +26,36 @@ TEST(RapidJSONTests, GenerateArray)
 	// check array size
 	ASSERT_EQ(3, document.Size());
 
-	stringstream strm;
 	FileStream f(stdout);
 	Writer<FileStream> writer(f);
 	document.Accept(writer);
+}
+
+TEST(RapidJSONTests, CreateInMemoryStream)
+{
+	Document document;
+	Document::AllocatorType& allocator = document.GetAllocator();
+
+	document.SetArray();
+	ASSERT_TRUE(document.IsArray());
+
+	document.PushBack("Test1", allocator);
+	document.PushBack("Test2", allocator);
+
+	Value item;
+	item.SetObject();
+	item.AddMember("Id", 1, allocator);
+	item.AddMember("Name", "ARInside", allocator);
+	document.PushBack(item, allocator);
+
+	// check array size
+	ASSERT_EQ(3, document.Size());
+
+	stringstream strm;
+	GenericWriteStream output(strm);
+	Writer<GenericWriteStream> writer(output);
+	document.Accept(writer);
+
+	string result = strm.str();
+	ASSERT_EQ("[\"Test1\",\"Test2\",{\"Id\":1,\"Name\":\"ARInside\"}]", result);
 }
