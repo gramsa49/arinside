277b85d43e464e2868e1973861512ff0bf2d513c
jls17 <jls17@1b5bf968-bca2-4794-aaf2-a93241d7ae32>
2013-08-01 22:19:15 +0000

    - updated test of GetExecutableDirectory - added missing lib to CMakeLists.txt so it's compilable on windows again
    git-svn-id: http://svn.arinside.org/branches/work_304@327 1b5bf968-bca2-4794-aaf2-a93241d7ae32

--- FileSystemUtilTest.cpp
+++ FileSystemUtilTest.cpp
@@ -1,5 +1,6 @@
 #include "ARInsideTest.h"
 #include "FileSystemUtil.h"
+#include "FileSystemUtilTest.h"
 
 TEST(FileSystemUtil, IsDots_WithDots_Test)
 {
@@ -16,5 +17,13 @@ TEST(FileSystemUtil, GetExecutableDirectory)
 {
 	string directory = FileSystemUtil::GetExecutableDirectory(argv_0);
 	directory = FileSystemUtil::GetRealPathName(directory);
-	cout << "CurrentDir: " << directory << endl;
+
+	string expectedDir = DIRECTORY_OF_TEST_BINARY;
+#ifdef WIN32
+	expectedDir = CUtil::StrReplace("/","\\", expectedDir);
+	std::transform(directory.begin(), directory.end(), directory.begin(), ::tolower);
+	std::transform(expectedDir.begin(), expectedDir.end(), expectedDir.begin(), ::tolower);
+#endif
+
+	ASSERT_STREQ(expectedDir.c_str(), directory.c_str());
 }
