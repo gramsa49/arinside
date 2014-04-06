9f313934d248086a0f279882fc670bdf909f6498
jls17 <jls17@1b5bf968-bca2-4794-aaf2-a93241d7ae32>
2014-04-06 22:12:02 +0000

    - fixed a problem with the field filtering on the schema page (column detection for vendor and view forms didn't work anymore)
    git-svn-id: http://svn.arinside.org/branches/work_311@511 1b5bf968-bca2-4794-aaf2-a93241d7ae32

--- res/schema_page.js
+++ res/schema_page.js
@@ -146,7 +146,7 @@ $('document').ready(function() {
 			var search = $("#fieldNameFilter").val().replace(" +", " ").replace(" ", ".*");
 			var numSearch = search.search("^\\d+$");
 			var matches = 0;
-			var hasRealFieldColumn = ($('#' + table_name + ' tbody tr:eq(0) th:eq(3)').text().lastIndexOf("Real Field", 0) === 0);
+			var hasRealFieldColumn = ($('#' + table_name + ' thead tr:eq(0) th:eq(3)').text().lastIndexOf("Real Field", 0) === 0);
 
 			$('#' + table_name + ' tbody').remove();
 
